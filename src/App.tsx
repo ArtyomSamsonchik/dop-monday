674fe818683e10c3b4942d0de4cc69a1496bf663
ArtyomSamsonchik <colpachek@gmail.com>
2022-08-03 18:57:43 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Button} from "./components/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type getType = {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
+
+export function App() {
+    const [get, setGet] = useState<Array<getType>>([]);
+
+    const getRequestHandler = () => {
+        setGet([]);
+    };
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setGet(json));
+    }, []);
+
+    return (
+        <div className="App">
+            <Button nickName={"CleanPage"} callback={getRequestHandler}/>
+            <p></p>
+            <ul>
+                {get.map(el => {
+                    return (
+                        <li key={el.id}>
+                            <h4>{el.userId}</h4>
+                            <p>{el.userId}</p>
+                            <p>{el.title}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type ButtonPropsType = {
+    nickName: string
+    callback: () => void
+}
+
+export const Button = (props: ButtonPropsType) => {
+    const onClickButtonHandler = () => {
+        props.callback();
+    };
+
+    return (
+        <button onClick={onClickButtonHandler}>{props.nickName}</button>
+    );
+};
